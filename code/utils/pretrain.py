dadbf75ae25a4f08ca9dc939c191d0cc2fa7ad0a
limeng <65967004+limengran98@users.noreply.github.com>
2023-03-02 17:55:36 +0800

    Add files via upload

---
+++ code/utils/pretrain.py
@@ -0,0 +1,113 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torch.utils.data import DataLoader
+from torch.optim import Adam, SGD
+from torch.nn import Linear
+from torch.utils.data import Dataset
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import scipy.sparse as sp
+
+#torch.cuda.set_device(3)
+
+
+class AE(nn.Module):
+    def __init__(self, n_enc_1, n_enc_2, n_enc_3, n_dec_1, n_dec_2, n_dec_3,
+                 n_input, n_z):
+        super(AE, self).__init__()
+        # encoder
+        self.enc_1 = Linear(n_input, n_enc_1)
+        self.enc_2 = Linear(n_enc_1, n_enc_2)
+        self.enc_3 = Linear(n_enc_2, n_enc_3)
+        # extracted feature by AE
+        self.z_layer = Linear(n_enc_3, n_z)
+        # decoder
+        self.dec_1 = Linear(n_z, n_dec_1)
+        self.dec_2 = Linear(n_dec_1, n_dec_2)
+        self.dec_3 = Linear(n_dec_2, n_dec_3)
+        self.x_bar_layer = Linear(n_dec_3, n_input)
+    def forward(self, x):
+        enc_z2 = F.relu(self.enc_1(x))
+        enc_z3 = F.relu(self.enc_2(enc_z2))
+        enc_z4 = F.relu(self.enc_3(enc_z3))
+        z = self.z_layer(enc_z4)
+        dec_z2 = F.relu(self.dec_1(z))
+        dec_z3 = F.relu(self.dec_2(dec_z2))
+        dec_z4 = F.relu(self.dec_3(dec_z3))
+        x_bar = self.x_bar_layer(dec_z4)
+
+        return x_bar, z
+
+
+class LoadDataset(Dataset):
+    def __init__(self, data):
+        self.x = data
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, idx):
+        return torch.from_numpy(np.array(self.x[idx])).float(), \
+               torch.from_numpy(np.array(idx))
+
+
+def adjust_learning_rate(optimizer, epoch):
+    lr = 0.001 * (0.1 ** (epoch // 20))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def pretrain_ae(model, feat,):
+    print(model)
+    optimizer = Adam(model.parameters(), lr=1e-3)
+    for epoch in range(30):
+        # adjust_learning_rate(optimizer, epoch)
+
+        x = feat.cuda()
+
+        x_bar, _ = model(x)
+        loss = F.mse_loss(x_bar, x)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print('{} loss: {}'.format(epoch, loss)) 
+
+        torch.save(model.state_dict(), 'aminer.pkl')
+
+def preprocess_features(features):
+    """Row-normalize feature matrix and convert to tuple representation"""
+    rowsum = np.array(features.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    features = r_mat_inv.dot(features)
+    return features.todense()
+
+
+path = "/home/zhaojy/data/acm/"
+feat_p = sp.load_npz(path + "p_feat.npz")
+feat = torch.FloatTensor(preprocess_features(feat_p)).cuda()
+
+path = "/home/zhaojy//data/aminer/"
+feat_p = sp.eye(6564)
+feat = torch.FloatTensor(preprocess_features(feat_p)).cuda()
+
+
+#path = "/home/zhaojy//data/freebase/"
+#feat_p = sp.eye(3492)
+#feat = torch.FloatTensor(preprocess_features(feat_p)).cuda()
+model = AE(
+            n_enc_1=500,
+            n_enc_2=500,
+            n_enc_3=2000,
+            n_dec_1=2000,
+            n_dec_2=500,
+            n_dec_3=500,
+            n_input=feat.shape[1],
+            n_z=10).cuda()
+pretrain_ae(model,feat)
